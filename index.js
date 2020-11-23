9b942d17e2b6b7e6bf5957127f512d201b972d05
Yvonne M Pham <yvonnersz@gmail.com>
2020-11-23 23:10:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 // array = [2, 3, 4, 5, 6, 7]
 
 function findMinAndRemove(array){
-  let newArray = new Array(Math.min(array));
-  binding.pry
-  return newArray
+  let lowestNumberArray = [];
+  return lowestNumberArray.push(Math.min(array));
 }
 
 function insertionSort(array){
