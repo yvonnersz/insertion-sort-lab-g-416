188c62a5d3ad7d73f634c66aca89b2f0145c5227
Yvonne <yvonnersz@gmail.com>
2020-12-02 22:14:04 -0800

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-function findMinAndRemove(array){
+function findMinAndRemove(array) {
+    let min = array[0];
+    let index = 0;
 
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] < min) {
+            min = array[i];
+            index = i;
+        }
+    }
+
+    array.splice(index, 1);
+    return min
 }
 
 function insertionSort(array){
+    let newMin;
+    let sortedArray = [];
 
+    while (array.length != 0) {
+        newMin = findMinAndRemove(array);
+        sortedArray.push(newMin)
+    }
+
+    return sortedArray
 }
